46e1e542a97ddd564dbe7d49577d41be9fcf5dcf
kri1107 <kripermana1107@gmail.com>
2023-02-06 14:10:07 +0700

    Added login screen

---
+++ app/src/main/java/com/androweed/muslimsimple/LoginActivity.kt
@@ -0,0 +1,26 @@
+package com.androweed.muslimsimple
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.androweed.muslimsimple.databinding.ActivityLoginBinding
+import android.text.Html
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val textView = binding.tvbLoginRegister
+        textView.text=Html.fromHtml("<font color=#ADADAD>Doesn't have an account? </font>" + "<font color=#2ABA7E>register now</font>" )
+
+        binding.tvbLoginRegister.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
