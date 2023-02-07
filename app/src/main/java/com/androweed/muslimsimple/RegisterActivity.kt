8e4ac16d1d97b127740d7dc7dd14d05a5b8d71f9
kri1107 <kripermana1107@gmail.com>
2023-02-07 14:07:48 +0700

    Updated, Added Firebase into register activity

--- app/src/main/java/com/androweed/muslimsimple/RegisterActivity.kt
+++ app/src/main/java/com/androweed/muslimsimple/RegisterActivity.kt
@@ -5,14 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html
 import com.androweed.muslimsimple.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        firebaseAuth = FirebaseAuth.getInstance()
 
         val textView = binding.tvbRegisterLogin
         textView.text= Html.fromHtml("<font color=#ADADAD>Already have account? </font>" + "<font color=#2ABA7E>login here</font>" )
@@ -20,6 +27,9 @@ class RegisterActivity : AppCompatActivity() {
         binding.tvbRegisterLogin.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.btnRegister.setOnClickListener {
         }
 
     }
