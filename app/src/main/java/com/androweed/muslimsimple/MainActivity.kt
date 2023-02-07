5ad90aafe160dc601ce8fdde625b39c40fbf3097
kri1107 <kripermana1107@gmail.com>
2023-02-07 14:06:51 +0700

    Added textview, dev mode watermarking

--- app/src/main/java/com/androweed/muslimsimple/MainActivity.kt
+++ app/src/main/java/com/androweed/muslimsimple/MainActivity.kt
@@ -2,6 +2,7 @@ package com.androweed.muslimsimple
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.androweed.muslimsimple.databinding.ActivityMainBinding
 
@@ -19,5 +20,15 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+        binding.btnHome.setOnClickListener{
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+        var textView: TextView
+
+        textView = binding.tvMainDevMode
+
+        textView.setText("This is developer mode main activity view\n" +
+        "All activity can be accessed here!!!\n" + "Copyright 2023 Androweed Team")
     }
 }
