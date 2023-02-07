cc9c1b33e3057e6db7f5b62a9bdc6aacacdb7490
kri1107 <kripermana1107@gmail.com>
2023-02-07 14:54:59 +0700

    Added lorem ipsum text to QuranFragment.java

--- app/src/main/java/com/androweed/muslimsimple/ui/quran/QuranFragment.java
+++ app/src/main/java/com/androweed/muslimsimple/ui/quran/QuranFragment.java
@@ -12,6 +12,8 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.androweed.muslimsimple.databinding.FragmentQuranBinding;
 
+import org.w3c.dom.Text;
+
 public class QuranFragment extends Fragment {
 
     private FragmentQuranBinding binding;
@@ -24,10 +26,17 @@ public class QuranFragment extends Fragment {
         binding = FragmentQuranBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textQuran;
-        quranViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        /*final TextView textView = binding.textQuran;
+        quranViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);*/
+        final  TextView tvQuranTest = (TextView) binding.tvQuranTest;
+        tvQuranTest.setText(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ornare orci lorem, vel ullamcorper dui mattis a. Morbi sit amet venenatis arcu. Maecenas pulvinar aliquam bibendum. Curabitur nec vulputate arcu. Etiam euismod dolor eget lobortis bibendum. Phasellus in dolor at quam luctus interdum. Morbi scelerisque, sem et feugiat efficitur, nisi libero tincidunt erat, et feugiat massa quam sed augue. Phasellus in est ante. Aliquam aliquet in mi quis fringilla. Duis in lorem rhoncus, efficitur elit nec, convallis leo. Duis id ante lacus. Sed ullamcorper eleifend aliquam. Cras fermentum, nisi tempor bibendum semper, neque ex laoreet nisi, vitae pharetra libero metus eu eros. "
+                        + "\n\nVivamus at nunc laoreet, commodo sapien efficitur, vulputate nunc. Praesent ultrices consequat varius. Sed in ligula vitae eros imperdiet fringilla. Suspendisse elementum, velit id imperdiet suscipit, turpis justo congue velit, sit amet maximus eros dolor eu magna. Praesent id enim sed tortor sagittis sollicitudin. Aliquam ornare lacinia gravida. Suspendisse tempor, lectus at tempus viverra, nisl turpis volutpat neque, tincidunt hendrerit justo ex ac est. Donec quis maximus metus, nec ultricies mauris. Duis fringilla, libero sed tincidunt gravida, quam nisl venenatis ante, a porttitor sapien orci sed urna."
+                        + "\n\nCurabitur pharetra rutrum turpis eget blandit. Cras a dapibus nulla, quis sagittis neque. Donec a dui quis diam dapibus ornare eu a ex. Quisque malesuada vestibulum leo. Curabitur sodales enim ac arcu suscipit aliquam. Maecenas vulputate porta lacus in tincidunt. Curabitur et malesuada ligula. Aenean lacinia, ante in dignissim mollis, leo nisi congue dolor, nec auctor ex ipsum nec nulla. "
+        );
         return root;
     }
+
 
     @Override
     public void onDestroyView() {
