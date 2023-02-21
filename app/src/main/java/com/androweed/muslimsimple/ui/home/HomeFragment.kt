55c7210dd3fad518ffbe146ee21818034989c089
Fakhri Permana <kripermana1107@gmail.com>
2023-02-21 14:11:11 +0700

    Added arraylist for Video Kajian feature.

--- app/src/main/java/com/androweed/muslimsimple/ui/home/HomeFragment.kt
+++ app/src/main/java/com/androweed/muslimsimple/ui/home/HomeFragment.kt
@@ -7,11 +7,18 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.androweed.muslimsimple.R
 import com.androweed.muslimsimple.databinding.FragmentHomeBinding
+import com.androweed.muslimsimple.videokajian.home_recyclerview.AdapterVideoKajian
+import com.androweed.muslimsimple.videokajian.home_recyclerview.ModelVideoKajian
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+    lateinit var rvVKajian : RecyclerView
+
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -33,6 +40,38 @@ class HomeFragment : Fragment() {
             textView.text = it
         }*/
         return root
+
+        /* Recycler View*/
+        val lm = LinearLayoutManager(activity)
+        lm.orientation = LinearLayoutManager.HORIZONTAL
+
+        rvVKajian = root.findViewById(R.id.cv_home_videkajian)
+
+        val adapterVideoKajian = AdapterVideoKajian(ArrayVideoKajian,activity)
+        rvVKajian.setHasFixedSize(true)
+        rvVKajian.layoutManager = lm
+        rvVKajian.adapter = adapterVideoKajian
+
+
+    }
+
+    val ArrayVideoKajian : ArrayList<ModelVideoKajian>get() {
+
+        val arrayvideokajian = ArrayList<ModelVideoKajian>()
+
+        val videokajian1 = ModelVideoKajian()
+        videokajian1.titleVideoKajian = "Al-Quran Membersihkan Hati"
+        videokajian1.channelVideoKajian = R.string.yufid_tv.toString()
+        videokajian1.thumbnailVideoKajian = R.drawable.iv_alquran_membersihkan_hati
+
+        val videokajian2 = ModelVideoKajian()
+        videokajian2.titleVideoKajian = "Baca, Pahami, Amalkan!"
+        videokajian2.channelVideoKajian = R.string.firanda_andirja.toString()
+        videokajian2.thumbnailVideoKajian = R.drawable.iv_baca_pahami_lalu_amalkan
+
+        arrayvideokajian.add(videokajian1)
+
+        return arrayvideokajian
     }
 
     override fun onDestroyView() {
