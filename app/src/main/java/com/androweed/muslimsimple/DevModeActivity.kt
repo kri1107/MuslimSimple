5be00233962d9b2bf182cc81293caa5c27cd3c14
Fakhri Permana <kripermana1107@gmail.com>
2023-02-24 14:36:28 +0700

    Changes: Added permission ask for location_permission feature

--- app/src/main/java/com/androweed/muslimsimple/DevModeActivity.kt
+++ app/src/main/java/com/androweed/muslimsimple/DevModeActivity.kt
@@ -1,10 +1,15 @@
 package com.androweed.muslimsimple
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import com.androweed.muslimsimple.arahkiblat.ArahKiblatActivity
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.androweed.muslimsimple.databinding.ActivityDevModeBinding
 
 class DevModeActivity : AppCompatActivity() {
@@ -15,16 +20,16 @@ class DevModeActivity : AppCompatActivity() {
         binding = ActivityDevModeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnLogin.setOnClickListener{
+        binding.btnLogin.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
-        binding.btnHome.setOnClickListener{
+        binding.btnHome.setOnClickListener {
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
         }
         binding.btnCompass.setOnClickListener {
-            val intent = Intent(this, ArahKiblatActivity::class.java)
+            val intent = Intent(this, ActivityCompassKiblat::class.java)
             startActivity(intent)
         }
 
@@ -34,5 +39,29 @@ class DevModeActivity : AppCompatActivity() {
 
         textView.text = "This is developer mode main activity view\n" +
                 "All activity can be accessed here!!!\n" + "Copyright 2023 Androweed Team"
+
+
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    // Precise location access granted.
+                    Toast.makeText(this, "Successfully granted preciese location access", Toast.LENGTH_SHORT).show()
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    // Only approximate location access granted.
+                    Toast.makeText(this, "Successfully granted approximate location access", Toast.LENGTH_SHORT).show()
+                } else -> {
+                // No location access granted.
+                Toast.makeText(this, "Permission for Location was denied, please manually allow location for this app for more better of work.", Toast.LENGTH_SHORT).show()
+            }
+            }
+        }
+
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION))
     }
+
 }
