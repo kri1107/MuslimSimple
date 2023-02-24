0f7897b31218c8774bd99d0a28168bc18dd7108e
Fakhri Permana <kripermana1107@gmail.com>
2023-02-24 14:34:57 +0700

    Added firebase auth into RegisterActivity

--- app/src/main/java/com/androweed/muslimsimple/RegisterActivity.kt
+++ app/src/main/java/com/androweed/muslimsimple/RegisterActivity.kt
@@ -4,13 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html
+import android.util.Patterns
+import android.widget.Toast
 import com.androweed.muslimsimple.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
-    lateinit var auth: FirebaseAuth
+    private lateinit var auth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +34,50 @@ class RegisterActivity : AppCompatActivity() {
         binding.btnRegister.setOnClickListener {
             val email = binding.edtEmail.text.toString()
             val password = binding.edtPassword.text.toString()
+
+            // Validasi Email
+            if (email.isEmpty()){
+                binding.edtEmail.error = "Please fill the email section"
+                binding.edtEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            //Validasi isi Email
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                binding.edtEmail.error = "Email was not valid"
+                binding.edtEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            //Validasi Password
+            if (password.isEmpty()){
+                binding.edtPassword.error = "Please fill the password section"
+                binding.edtPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            //Validasi panjang Password
+            if (password.length < 6){
+                binding.edtPassword.error = "Password must more than 6 character"
+                binding.edtPassword.requestFocus()
+                return@setOnClickListener
+            }
+
+            RegisterFirebase(email,password)
         }
 
     }
+    private fun RegisterFirebase(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){
+                if (it.isSuccessful){
+                    Toast.makeText(this, "Successfully registered", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this,LoginActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this,"${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 }
